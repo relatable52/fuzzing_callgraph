7b5d8fc0dd63d1da0682250f588861336b1283c6
Chinh Tran <93480420+relatable52@users.noreply.github.com>
2025-03-24 23:44:25 +0700

    Test 1

--- testing/projects/metadata-extractor/prepare_corpus.sh
+++ testing/projects/metadata-extractor/prepare_corpus.sh
@@ -1,9 +1,12 @@
 CORPUS_DIR=$1
 
-cp $CORPUS/go-fuzz-corpus-master/bmp/corpus/* \
-    $CORPUS/go-fuzz-corpus-master/gif/corpus/* \
-    $CORPUS/go-fuzz-corpus-master/jpeg/corpus/* \
-    $CORPUS/go-fuzz-corpus-master/png/corpus/* \
-    $CORPUS/go-fuzz-corpus-master/tiff/corpus/* \
-    $CORPUS/go-fuzz-corpus-master/webp/corpus/* \
-    $CORPUS_DIR
+mkdir -p "$CORPUS_DIR"
+
+find $CORPUS/go-fuzz-corpus-master/bmp/corpus \
+     $CORPUS/go-fuzz-corpus-master/gif/corpus \
+     $CORPUS/go-fuzz-corpus-master/jpeg/corpus \
+     $CORPUS/go-fuzz-corpus-master/png/corpus \
+     $CORPUS/go-fuzz-corpus-master/tiff/corpus \
+     $CORPUS/go-fuzz-corpus-master/webp/corpus \
+     -type f ! -name "da39a3ee5e6b4b0d3255bfef95601890afd80709*" \
+     -exec cp {} "$CORPUS_DIR" \;
