ef6dc59ca40b2d9e0a6e5e0627c4ff20f6e40267
Chinh Tran <93480420+relatable52@users.noreply.github.com>
2025-03-27 01:38:10 +0700

    Test 1

--- testing/run_select.sh
+++ testing/run_select.sh
@@ -17,5 +17,15 @@ while IFS= read -r program || [[ -n "$program" ]]; do
     [[ -z "$program" ]] && continue
 
     # Run the just command
-    just --dotenv-path "$program/.env" projects/run_complete
+    just --dotenv-path "$program/.env" projects/coverage_seed
+    just --dotenv-path "$program/.env" projects/static_callgraphs
+    # Run fuzzing 3 times
+    for i in {1..3}; do
+        just --dotenv-path "$program/.env" projects/coverage_fuzzing
+    done
+
+    # Run fuzzing_seed 12 times
+    for i in {1..12}; do
+        just --dotenv-path "$program/.env" projects/coverage_fuzzing_seed
+    done
 done <"$TO_RUN"
