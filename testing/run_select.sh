52e7287348d0b07ba2c730153b23111b9ea96216
Chinh Tran <93480420+relatable52@users.noreply.github.com>
2025-03-27 00:01:53 +0700

    Test 1

--- testing/run_select.sh
+++ testing/run_select.sh
@@ -1,6 +1,7 @@
 while read program; do \
+    echo "Running $program"; \
     just \
     --justfile projects/justfile \
-    --dotenv-path projects/$program/.env \
+    --dotenv-path "projects/$program/.env" \
     run_complete; \
 done < projects/to_run.txt
