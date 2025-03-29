dceb4fe038ed8fa30fe506a2768596d3ac01d432
Chinh Tran <93480420+relatable52@users.noreply.github.com>
2025-03-29 23:33:06 +0700

    Test 1

--- testing/projects/jsoup/prepare_corpus.sh
+++ testing/projects/jsoup/prepare_corpus.sh
@@ -1,9 +1,10 @@
 CORPUS_DIR=$1
 
-mkdir -p "$CORPUS_DIR"
+mkdir -p "$CORPUS_DIR/xml"
+mkdir -p "$CORPUS_DIR/html"
 
-cp -r $CORPUS/go-fuzz-corpus/xml/corpus/* $CORPUS_DIR/xml
-cp -r $CORPUS/go-fuzz-corpus/htmltemplate/corpus/* $CORPUS_DIR/html
+cp -r $CORPUS/go-fuzz-corpus/xml/corpus/* $CORPUS_DIR/xml/
+cp -r $CORPUS/go-fuzz-corpus/htmltemplate/corpus/* $CORPUS_DIR/html/
 
-cp -r $CORPUS/fuzzing-corpus/xml/* $CORPUS_DIR/xml
-cp -r $CORPUS/fuzzing-corpus/html/* $CORPUS_DIR/html
+cp -r $CORPUS/fuzzing-corpus/xml/* $CORPUS_DIR/xml/
+cp -r $CORPUS/fuzzing-corpus/html/* $CORPUS_DIR/html/
