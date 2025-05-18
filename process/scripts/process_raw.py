aa29aa27ea61f8b72621bf6f03cfa9ee9448a526
Chinh Tran <93480420+relatable52@users.noreply.github.com>
2025-05-19 04:02:57 +0700

    Add process script

--- process/scripts/process_raw.py
+++ process/scripts/process_raw.py
@@ -37,6 +37,7 @@ def get_cg_paths(program: str) -> dict:
     output_dir = os.path.join(OUTPUT_DIR, program)
     os.makedirs(output_dir, exist_ok=True)
 
+    print(program_dir + "/**/fuzzing/**/cg.json")
     fuzzing_dyncg = glob(program_dir + "/**/fuzzing/**/cg.json", recursive=True)[0]
     print(fuzzing_dyncg)
     fuzzing_seed_dyncg = glob(
