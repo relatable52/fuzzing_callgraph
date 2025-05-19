ce2b90ad996248d62c6bc0fc8f7552c96b2baad7
Chinh Tran <93480420+relatable52@users.noreply.github.com>
2025-05-20 00:12:15 +0700

    Add process script

--- process/scripts/config.py
+++ process/scripts/config.py
@@ -4,15 +4,7 @@ RAW_CALLGRAPH = os.environ.get("CALLGRAPH", "/data/input")
 OUTPUT_DIR = os.environ.get("OUTPUT_DIR", "/data/output")
 CODE_DIR = os.environ.get("CODE_DIR", "/data/code")
 
-STATICCG = (
-    "OPAL/RTA",
-    "OPAL/CHA",
-    "OPAL/0-CFA",
-    "WALA/RTA",
-    "WALA/CHA",
-    "WALA/0-CFA",
-    "Soot/CHA",
-)
+STATICCG = ("OPAL/RTA", "OPAL/0-CFA", "WALA/0-CFA")
 
 PROGRAMS = (
     "apache-commons-jxpath",
